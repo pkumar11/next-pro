29b7dafcbbedf290cbcd4fb7f1c4675840a581d2
pankaj kumar <pankaj10101993@gmail.com>
2026-03-06 22:36:32 +0530

    sign up page created

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { signUpSchema } from '@/app/schemas/auth';
+import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import React from 'react';
+import { FieldGroup, Field, FieldLabel, FieldError } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
 
 function signUp() {
+     const form = useForm({
+    resolver: zodResolver(signUpSchema),//validate the form data against the zod schema
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
+  });
+
     return (
         <div className="py-12">
 
@@ -10,10 +26,68 @@ function signUp() {
           <CardTitle>Sign up</CardTitle>
           <CardDescription>Create a new account</CardDescription>
         </CardHeader>
-        <CardContent>
-      
-    
-        </CardContent>
+ <CardContent>
+        <form>
+          <FieldGroup className="gap-y-4">
+            <Controller
+              name="name"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Full Name</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Please enter your full name"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Email</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Please enter your email"
+                    type="email"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel>Password</FieldLabel>
+                  <Input
+                    aria-invalid={fieldState.invalid}
+                    placeholder="*****"
+                    type="password"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          <Button>Sign up</Button>
+           
+          </FieldGroup>
+        </form>
+      </CardContent>
       </Card>
     </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           ><main>
-          <Navbar />
+          {/* <Navbar /> */}
         </main></ThemeProvider>
        
         {children}
