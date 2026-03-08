eee7a2265bf78a3d98464e0cef8b7b1c9022380d
pankaj kumar <pankaj10101993@gmail.com>
2026-03-08 11:06:54 +0530

    blog page created with skeleton

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -2,9 +2,11 @@ import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
-import { Link } from "lucide-react";
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 import { cacheLife, cacheTag } from "next/cache";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function BlogPage() {
   return (
@@ -17,10 +19,13 @@ export default function BlogPage() {
           Insights, thoughts, and trends from our team.
         </p>
       </div>
-      {LoadBlogList()}
+      <Suspense fallback={<SkeletonLoadingUi />}>
+        <LoadBlogList />
+      </Suspense>
     </div>
   );
 }
+
 async function LoadBlogList() {
   "use cache";
   cacheLife("hours");
@@ -31,11 +36,9 @@ async function LoadBlogList() {
     <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       {data?.map((post) => (
         <Card key={post._id} className="pt-0">
-      
-  <div className="relative h-48 w-full overflow-hidden">
+          <div className="relative h-48 w-full overflow-hidden">
             <Image
               src={
-               
                 "https://images.unsplash.com/photo-1761019646782-4bc46ba43fe9?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               }
               alt="image"
@@ -43,6 +46,7 @@ async function LoadBlogList() {
               className="rounded-t-lg object-cover"
             />
           </div>
+
           <CardContent>
             <Link href={`/blog/${post._id}`}>
               <h1 className="text-2xl font-bold hover:text-primary">
@@ -62,6 +66,23 @@ async function LoadBlogList() {
             </Link>
           </CardFooter>
         </Card>
+      ))}
+    </div>
+  );
+}
+
+function SkeletonLoadingUi() {
+  return (
+    <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+      {[...Array(3)].map((_, i) => (
+        <div className="flex flex-col space-y-3" key={i}>
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/" />
+          </div>
+        </div>
       ))}
     </div>
   );
